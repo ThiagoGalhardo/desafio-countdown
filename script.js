6f0e0547d79769db649bb6b89a0161b650b8b5c6
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-03-02 22:39:05 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const countdown = () => {
+    const countDate = new Date("dec 31, 2022 23:59:59").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.getElementById("day_time").innerHTML = textDay;
+    document.getElementById("hour_time").innerHTML = textHour;
+    document.getElementById("minute_time").innerHTML = textMinute;
+    document.getElementById("second_time").innerHTML = textSecond;
+
+    console.log(textDay);
+    console.log(textHour);
+    console.log(textMinute);
+    console.log(textSecond);
+}
+
+setInterval(countdown, 1000);
